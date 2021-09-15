779c5304150d02b78a6ce650ed138e03f421faa2
Alvaro Obyrne <alvarobyrne@gmail.com>
2021-09-14 20:36:41 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var easymidi = require('easymidi');
+console.log('easymidi: ', easymidi);
+var inputs = easymidi.getInputs();
+console.log('inputs: ', inputs);
+/*
+var virtualOutput = new easymidi.Output('NodeVirtual', true);
+setInterval(function() {
+console.log('sending',Math.random());
+virtualOutput.send('noteon', {
+  note: 64,
+  velocity: 127,
+  channel: 0
+});
+}, 1000);
+*/
+
+//'nanoKONTROL2 0' 
+var input = new easymidi.Input('nanoKONTROL2 0');
+input.on('noteon', function (msg) {
+    console.log('msg: ', msg);
+  // do something with msg
+});
