1f6c400e483626ba343b9181bf731397693f38d2
Alvaro Obyrne <alvarobyrne@gmail.com>
2021-09-17 10:22:41 -0500

    virtual port name changed on performance file

--- MIDIMAPCODE2.js
+++ MIDIMAPCODE2.js
@@ -17,15 +17,21 @@ midi.inputs.forEach((a,k,i)=>{
 
 akai = midi.inputs.get("-65886926")
 
-nodeID=Array.from(midi.inputs).filter(input=>input[1].name=="NodeVirtual")[0][1].id
+virtualOutputName= 'NodeVirtualOutput';//Should match the name of the virtual output in the `bridge.js` file
+nodeID=Array.from(midi.inputs).filter(input=>input[1].name===virtualOutputName)[0][1].id
 node = midi.inputs.get(nodeID)
 
 sincAbleton = Array(128).fill(0)
+abletonVelocity = -1;
+abletonNote=-1;
 node.onmidimessage=function (e){
   let data = e.data
+  let type = data[0];
   let index = data[1];
-  let datum = sincAbleton[index] = data[2];
-  console.log('index : ',index, 'datum : ', datum);
+  abletonNote = index;
+  let velocity = sincAbleton[index] = data[2];
+  abletonVelocity=velocity;
+  console.log('type: ', type,'index : ',index, 'velocity : ', velocity);
 }
 
 sincAkai = Array(128).fill(0)
@@ -35,49 +41,3 @@ akai.onmidimessage=function (e){
   let datum = sincAkai[index] = data[2];
   console.log('index : ',index, 'datum : ', datum);
 };
-
-
-osc(()=>sincAbleton[55]/4 , 0.8 , ()=>sincAkai[1]*6/127).out()
-osc(   100                , 0.1 ,     2                ).out()
-
-noise(()=>{
-return sincAkai[2]*2+1
-}).out()
-
-
-osc(
-100
-,
-0.1
-,
-2
-).out()
-
-osc(
-  ()=>{
-return sincAkai[1]*2+100
-}
-,
-()=>{
-return sincAkai[2]/127
-}
-,
-()=>{
-return sincAkai[3]*6/127
-})
-.out()
-
-noise(1,0.1)
-.color(()=>( sincAkai[1]*0.1+0.5),0.3,0.7)
-.invert(0.1)
-.rotate(2,0.1)
-.kaleid(()=>( sincAkai[2]*0.1+2/127))
-.add(shape(4,0.3,(()=>( sincAkai[3]*0.01+0.3))).modulate(o0)) // Neutron //
-.out()
-
-noise(  ()=>( sincAkai[1]*0.1+1),()=>( sincAkai[2]*0.5/127))
-.kaleid(()=>( sincAkai[3]*0.1+4/127))
-.color(()=>( sincAkai[6]*0.1+0.5),0.2,0.8)
-.add(noise(()=>( sincAkai[4]*0.5+3/127),()=>( sincAkai[5]*0.5+10/127))
-.color(()=>sincAkai[7]*0.1,0.5,1))
-.out()
