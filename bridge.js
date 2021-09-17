e3a983401a888ae6708223583584ff60e5c17d77
Alvaro Obyrne <alvarobyrne@gmail.com>
2021-09-17 10:20:31 -0500

    virtual port name and noteoff added

--- bridge.js
+++ bridge.js
@@ -1,8 +1,18 @@
 var easymidi = require('easymidi');
+const virtualOutputName= 'NodeVirtualOutput';//Should be matched at performance file
 var input = new easymidi.Input('VirtualJavascript',true);
-var virtualOutput = new easymidi.Output('NodeVirtual', true);
+var virtualOutput = new easymidi.Output(virtualOutputName, true);
 input.on('noteon', function (msg) {
     virtualOutput.send('noteon', {
+        note: msg.note,
+        velocity: msg.velocity,
+        channel: 0
+    });
+
+});
+input.on('noteoff', function (msg) {
+    console.log('msg: ', msg);
+    virtualOutput.send('noteoff', {
         note: msg.note,
         velocity: msg.velocity,
         channel: 0
